35e16f197a9a1449941198985bd4d926f6c4d281
Андрей Милютин <andrew.milyutin@gmail.com>
2021-10-12 20:18:10 +0300

    version 5

--- scripts/index.js
+++ scripts/index.js
@@ -3,50 +3,51 @@ const editBtnPosts = document.querySelector('.profile__button');
 const popup = document.querySelector('.popup');
 const form = document.querySelector('.form');
 const closeBtnProfile = document.querySelector('.form__close_profile');
+const formPosts = document.querySelector('.form-posts');
+const closeBtnPosts = document.querySelector('.form__close_posts');
+
+function openModalWindow(modalWindow, addClass) {
+  modalWindow.classList.add(addClass)
+}
+
+function closeModalWindow(modalWindow, remClass) {
+  modalWindow.classList.remove(remClass)
+}
 
 /*______ открыть/закрыть профиль ______*//*______ открыть/закрыть профиль ______*//*______ открыть/закрыть профиль ______*//*______ открыть/закрыть профиль ______*/
 
 function openClickProfile() {
-    form.classList.add('form_opened');
-    popup.classList.add('popup_opened');
-  }
-
-editBtn.addEventListener('click', openClickProfile);
+  openModalWindow(form, 'form_opened')
+  openModalWindow(popup, 'popup_opened')
+}
 
 function closeClickProfile() {
-    form.classList.remove('form_opened');
-    popup.classList.remove('popup_opened');
-  }
-
+  closeModalWindow(form, 'form_opened')
+  closeModalWindow(popup, 'popup_opened')
+}
+editBtn.addEventListener('click', openClickProfile);
 closeBtnProfile.addEventListener('click', closeClickProfile);
 
 /*______ открыть/закрыть посты ______*//*______ открыть/закрыть посты ______*//*______ открыть/закрыть посты ______*//*______ открыть/закрыть посты ______*/
 
-const formPosts = document.querySelector('.form-posts');
-const closeBtnPosts = document.querySelector('.form__close_posts');
-
 function openClickPosts() {
-    formPosts.classList.add('form_opened');
-    popup.classList.add('popup_opened');
-  }
-
-editBtnPosts.addEventListener('click', openClickPosts);
+  openModalWindow(formPosts, 'form_opened')
+  openModalWindow(popup, 'popup_opened')
+}
 
 function closeClickPosts() {
-    formPosts.classList.remove('form_opened');
-    popup.classList.remove('popup_opened');
-  }
-
+  closeModalWindow(formPosts, 'form_opened')
+  closeModalWindow(popup, 'popup_opened')
+}
+editBtnPosts.addEventListener('click', openClickPosts);
 closeBtnPosts.addEventListener('click', closeClickPosts);
 
 /*______ создание постов ______*//*______ создание постов ______*//*______ создание постов ______*//*______ создание постов ______*/
 
-const posts = document.querySelector('.posts');
 const postTitle = document.querySelector('.input__text_type_title');
 const postImage = document.querySelector('.input__text_type_image');
-const body = document.querySelector('body');
 
-function addPost(title, image){
+function createCard(title, image) {
   const postsTemplate = document.querySelector('#post-template').content;
   const postsEl = postsTemplate.querySelector('.posts__post').cloneNode(true);
 
@@ -58,30 +59,36 @@ function addPost(title, image){
     evt.target.classList.toggle('posts__like_active');
   });
 
-  postsEl.querySelector('.posts__image').onclick = function(){
+  postsEl.querySelector('.posts__image').addEventListener('click', function () {
     containerImage.style.display = "flex";
     targetImage.src = this.src;
     contentText.textContent = this.alt;
-    popup.classList.add('popup_opened');
-  }
+    openModalWindow(popup, 'popup_opened')
+  });
 
-  posts.prepend(postsEl);
+  postsEl.querySelector('.posts__rubbish').addEventListener('click', function (evt) {
+    evt.target.parentNode.remove();
+  });
+
+  return postsEl;
 }
 
-const addPostButton = document.querySelector('.form__type_save_posts');
+function addCard(container, cardElement) {
+  container.prepend(cardElement);
+}
 
+const posts = document.querySelector('.posts');
+const addPostButton = document.querySelector('.form__type_save_posts');
 const formPostsElement = document.querySelector('.form-posts');
 
-function addPostsFormSubmit (evt) {
-  evt.preventDefault(); 
-  addPost(postTitle.value, postImage.value);
-  formPosts.classList.remove('form_opened');
-  popup.classList.remove('popup_opened');
+function addPostsFormSubmit(evt) {
+  evt.preventDefault();
+  addCard(posts, createCard(postTitle.value, postImage.value));
+  closeClickPosts()
 
   postTitle.value = '';
   postImage.value = '';
 }
-
 formPostsElement.addEventListener('submit', addPostsFormSubmit);
 
 /*______ автоматическое добавление постов ______*//*______ автоматическое добавление постов ______*//*______ автоматическое добавление постов ______*//*______ автоматическое добавление постов ______*/
@@ -111,70 +118,40 @@ const initialPosts = [
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
-  ];
-
-for(let i = 0; i < initialPosts.length; i++){
-  addPost(initialPosts[i].name, initialPosts[i].link);
-}
+];
+initialPosts.forEach(function (post) {
+  addCard(posts, createCard(post.name, post.link));
+})
 
 /*______ Открыть картинку ______*//*______ Открыть картинку ______*//*______ Открыть картинку ______*//*______ Открыть картинку ______*/
 
-let containerImage = document.getElementById('big-post');
-let images = document.getElementsByClassName('posts__image');
-let targetImage = document.getElementById("bigImage");
-let contentText = document.getElementById("big-post__text");
+const containerImage = document.getElementById('big-post');
+const targetImage = document.getElementById("bigImage");
+const contentText = document.getElementById("big-post__text");
+const button = document.getElementById("postClose");
 
-let button = document.getElementsByClassName("big-post__close")[0];
-
-button.onclick = function() { 
+button.addEventListener("click", function () {
   containerImage.style.display = "none";
-  popup.classList.remove('popup_opened');
+  closeModalWindow(popup, 'popup_opened')
   targetImage.src = '';
   contentText.textContent = '';
-}
-
-/*______ удаление постов ______*//*______ удаление постов ______*//*______ удаление постов ______*//*______ удаление постов ______*/
-
-function delPost(post, attribute, name) {
-    return function(i) {
-      let target = i.target;
-      if (!(target.hasAttribute(attribute) ? (target.getAttribute(attribute) === name ? true : false) : false)) return;
-      while (target != this) {
-        if (target.classList.contains(post)) {
-          target.remove();
-          return;
-        }
-        target = target.parentNode;
-      }
-      return;
-    };
-  }
-  
-document.addEventListener("click", delPost("posts__post", "delete", "remove"));
+});
 
 /*______ Редактирование профиля ______*//*______ Редактирование профиля ______*//*______ Редактирование профиля ______*//*______ Редактирование профиля ______*/
 
-const editProfileButton = document.querySelector('.form__type_save_profile');
 const editprofileName = document.querySelector('.input__text_name');
 const editprofileStatus = document.querySelector('.input__text_status');
-
-let profileName = document.querySelector('.profile__name');
-let profileStatus = document.querySelector('.profile__status');
-
-editprofileName.value = 'Жак-Ив Кусто';
-editprofileStatus.value = "Исследователь океана";
-profileName.textContent = 'Жак-Ив Кусто';
-profileStatus.textContent = "Исследователь океана";
-
+const profileName = document.querySelector('.profile__name');
+const profileStatus = document.querySelector('.profile__status');
 const formElement = document.querySelector('.form');
 
-function formSubmitHandler (evt) {
-  evt.preventDefault(); 
+editprofileName.value = profileName.textContent;
+editprofileStatus.value = profileStatus.textContent;
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
   profileName.textContent = editprofileName.value;
   profileStatus.textContent = editprofileStatus.value;
-
-  form.classList.remove('form_opened');
-  popup.classList.remove('popup_opened');
+  closeClickProfile()
 }
-
 formElement.addEventListener('submit', formSubmitHandler);
